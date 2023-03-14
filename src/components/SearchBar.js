e97fc2d145d56886d478e98aed347030ba7d39f2
rajatmungra <102897093+rajatmungra@users.noreply.github.com>
2023-03-14 16:00:11 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -21,7 +21,7 @@ class SearchBar extends React.Component{
             <div className="search-bar ui segment">
                 <form onSubmit={this.onFormSubmit}className="ui form">
                     <div className="field">
-                        <label>Video Search</label>
+                        <label>Video Search 😀 </label>
                         <input value={this.state.term} type="text" onChange={this.onTextChange}/>
                     </div>
                 </form>
